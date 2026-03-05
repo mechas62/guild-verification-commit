4db5217d676fbd68abf91f57468f5c0d982f413f
mesut canlı <mstcnl62@gmail.com>
2026-03-05 23:56:28 +0300

    Add dark mode support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:56:19.546Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T20:56:29.922Z
+Commit: Add dark mode support
