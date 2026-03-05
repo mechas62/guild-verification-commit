b08de4b76ab87165dc1b706e204b07b2e00fbfab
mesut canlı <mstcnl62@gmail.com>
2026-03-05 23:56:30 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:56:22.874Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T20:56:32.275Z
+Commit: Fix broken links in documentation
